e45e35f503c27379d56805cad5be4f136ada5d12
Ahmad-sca <121244931+Ahmad-sca@users.noreply.github.com>
2023-02-07 17:49:12 +0700

    Add files via upload
    YouTube api iframe

---
+++ YouTubeApi3D.js
@@ -0,0 +1,113 @@
+$(function() {
+
+    let searchTerm = "";
+    let videoList = $("#videoList");
+    let apiKey = "AIzaSyAjAbGHvlLm9UlEAVTIIFaZQxkMAYX9OlI";
+    let player = $("#player");
+    
+    clearVideoList();
+    search(searchTerm);
+    
+    $("#searchForm").on("submit", (e) => {
+        e.preventDefault();
+        searchTerm = $("#searchField").val();
+        search(searchTerm);
+    });
+    function search(q) {
+        $.ajax({
+            type: "GET",
+            url: `https://www.googleapis.com/youtube/v3/search?part=snippet&q=${searchTerm}&type=video&key=${apiKey}`,
+            success: function(data) {
+              console.log(data.items[0].snippet.title);
+            },
+            data: {
+                key: apiKey,
+                q: q,
+                part: "snippet",
+                maxResults: 10,
+                type: "video",
+                videoEmbeddable: true,
+                //fields: "items(snippet(title,publishedAt))",
+                //videoDefinition: "high",
+                videoDimension: "3d",
+               // videoDuration: "short",
+            },
+           /* success: function(response) {
+                // do something with the response
+                var videos = response.items;
+                // loop through the videos array and display the title and published date
+                for (var i = 0; i < videos.length; i++) {
+                    console.log(videos[i].snippet.title + " - " + videos[i].snippet.publishedAt);
+                }
+            },
+            error: function(error) {
+                console.log(error);
+            }*/
+        }).done((data) => {
+            let videos = data.items;
+            clearVideoList();
+            videos.forEach((el) => {
+                //add channelTitle
+                // add video id to li element
+                videoList.append(`<li loading="lazy" onclick="$('html, body').animate({ scrollTop: 0 }, 'slow');" class="media mb-2" id="${el.id.videoId}">
+                <img loading="lazy" src="${el.snippet.thumbnails.medium.url}" class="mr-3">
+                <div class="media-body">
+                  <h5 class="card-title mt-0 mb-1">${el.snippet.title}</h5>
+                  <h6 class="my-1 text-info">${el.snippet.channelTitle}</h6>
+                </div>
+              </li>`);
+            });
+
+            //By default, the first video of the search result will be played
+            //display the title, the description and the channel name
+            let video = data.items[0];
+            play(video.id.videoId, video.snippet.title,
+                video.snippet.channelTitle);
+
+        }).fail(function(data) {
+            console.log(data);
+        });
+    }
+
+    function clearVideoList() {
+        videoList.find(".media").remove();
+    }
+
+   async function play(id, title, channelTitle) {
+        player.attr("src", `https://www.youtube.com/embed/${id}?enablejsapi=1?&vq=small`);
+        $("#video-title").text(title);
+        $("#video-channelTitle").text(channelTitle);
+        //$("#youtubelink").attr("value", `https://youtu.be/${id}`)
+        
+    }
+
+    //select the video
+    videoList.on("click", "li", function() {
+        let id = $(this).attr("id");
+        findVideoById(id);
+    });
+
+    //find video by id
+   async function findVideoById(id) {
+        $.ajax({
+            method: "GET",
+            url: "https://www.googleapis.com/youtube/v3/videos",
+            
+            data: {
+                key: apiKey,
+                id: id,
+                part: "snippet",
+            }
+        }).done((data) => {
+            let video = data.items[0];
+            let snippet = video.snippet;
+            play(id, snippet.title, snippet.channelTitle)
+        }).fail((data) => {
+            console.log(data);
+        });
+
+    }
+    
+
+});
+
